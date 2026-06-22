7261938567d07af6fb49cc7ad98ce4d355192bc9
Sai-Kumar-Vadicherla <saikumarvadi@gmail.com>
2026-06-22 13:44:04 +0530

    Create AsciiExample.java

---
+++ java_practice/src/AsciiExample.java
@@ -0,0 +1,15 @@
+public class AsciiExample {
+    public static void main(String[] args) {
+        /* 
+        
+        American Standard Code for Information Interchange.
+        
+        */
+        char ch ='A';
+        System.out.println((int)ch);
+        
+        char c = 97;
+        
+        System.out.println(c);
+    }
+}
