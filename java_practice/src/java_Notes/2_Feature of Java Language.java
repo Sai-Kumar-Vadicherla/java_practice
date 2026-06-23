14ac6371a0ab076924e582de0c65997acb3dbb33
Sai-Kumar-Vadicherla <saikumarvadi@gmail.com>
2026-06-23 22:21:38 +0530

    Document major features of Java language
    Added a list of major features of Java language.

--- java_practice/src/java_Notes/2_Feature of Java Language.java
+++ java_practice/src/java_Notes/2_Feature of Java Language.java
@@ -1,4 +1,15 @@
 Feature of Java Language
   Java is one of the most popilar programming languages because it provides many powerful features. These features make java      simple. secure, portable and suitable for developing different types of applications
 The major features of Java are:
- 
+  1. Simple
+  2. Object-Oriented
+  3. Platform Independent
+  4. Portable
+  5. Secure
+  6. Robust
+  7. Architecture Neutral
+  8. High Performance
+  9. Multithreaded
+  10. Distributed
+  11. Dynamic
+  12. Compiled and Interpreted
