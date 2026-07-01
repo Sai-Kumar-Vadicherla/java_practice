466cd7f0f5b711baa4d2a0ed7e5ff6c322f43ba2
Sai-Kumar-Vadicherla <saikumarvadi@gmail.com>
2026-07-01 21:56:54 +0530

    Add example of lambda expression in Java
    Demonstrate the use of lambda expressions in Java by replacing an anonymous class implementation with a lambda expression.

---
+++ java_practice/src/Lambda_Expression_In_Java.java
@@ -0,0 +1,29 @@
+// Title: Lambda Expression in Java
+
+@FunctionalInterface
+interface A {
+    void show();
+}
+
+public class Lambda_Expression_In_Java {
+
+    public static void main(String[] args) {
+
+        /*
+         * Before Lambda Expression
+         *
+         * A obj = new A() {
+         *     @Override
+         *     public void show() {
+         *         System.out.println("Hi from B");
+         *     }
+         * };
+         */
+
+        // After Lambda Expression
+
+        A obj = () -> System.out.println("Hi from B");
+
+        obj.show();
+    }
+}
